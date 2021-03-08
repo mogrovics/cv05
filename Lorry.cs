e8cd19cfafffa603e12992e17223e9e3a5d33773
mogrovics <alex.mogrovics@gmail.com>
2021-03-08 21:10:38 +0100

    Add project files.

---
+++ Lorry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv05
+{
+    class Lorry : Vehicle
+    {
+        // max load capacity
+        private double maxLoad;
+        // current load capacity
+        private double load;
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv05
+{
+    public class Vehicle
+    {
+        // max amount of fuel
+        private double tankSize;
+        // current amount of fuel
+        private double fuel;
+
+        public enum FuelType { Gasoline, Diesel }
+    
+    }
+}
